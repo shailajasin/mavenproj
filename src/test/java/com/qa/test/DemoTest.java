6bd655c6dbedaa350fa0b55021ce57a0fbe050e7
shailu <shailaja.s1@gmail.com>
2019-05-08 18:48:16 -0400

    maven project

---
+++ src/test/java/com/qa/test/DemoTest.java
@@ -0,0 +1,25 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void Sum()
+	{
+		System.out.println("SUM");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(30,a+b);
+	}
+	
+	@Test
+	public void Sub()
+	{
+		System.out.println("SUB");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(10,b-a);
+	}
+}
